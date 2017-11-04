d9d7e3decf2a26a13afff2b3a4eda3bea7286c92
Vandan Patel <VandanPatel@Mains-MacBook-Pro.local>
2017-11-04 11:32:05 -0400

    Finished our VPFramework

---
+++ VPFramework/VPFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  VPFramework
+//
+//  Created by Vandan Patel on 11/4/17.
+//  Copyright © 2017 Vandan Patel. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Did some stuff, here you go homeboy!"
+    }
+}
---
+++ VPFramework/VPFramework/VPFramework.h
@@ -0,0 +1,19 @@
+//
+//  VPFramework.h
+//  VPFramework
+//
+//  Created by Vandan Patel on 11/4/17.
+//  Copyright © 2017 Vandan Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VPFramework.
+FOUNDATION_EXPORT double VPFrameworkVersionNumber;
+
+//! Project version string for VPFramework.
+FOUNDATION_EXPORT const unsigned char VPFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VPFramework/PublicHeader.h>
+
+
